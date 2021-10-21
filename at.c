d3ab09a2c2f58e74a8592e355d24a2dc5f6bd091
Alexandru Contra <contraalex8@gmail.com>
2021-10-21 16:52:06 +0300

    states injt

---
+++ at.c
@@ -0,0 +1,16 @@
+#include<stdint.h>
+uint32_t state =0; // we will store this var in heap because we need everytime to know the last state
+int32_t validator(uint8_t character){
+    int32_t result = 2;  //sustain the state to be in process
+
+    //RULES:  
+    /*
+        Result will contain everytime a value which will be returned in our main call
+        0- correct message 
+        1- incorrect message 
+        if there is no output between 0 or 1 it's not done
+    */
+    //implement of the switch statement.
+
+    return result;
+}
---
+++ at.h
@@ -0,0 +1,13 @@
+#ifndef __AT_H
+#define __AT_H
+
+#include<stdint.h>
+#define MAX_STRING_COUNTER 1000 //it will contain a max of 1000 strings
+#define MAX_STRING_LENGHT 128 // max length of the string will be 128
+typedef struct {
+    uint8_t ok; //if ok = 1 means ok , if ok = 0 means error
+    uint32_t line_counter; // it will count how much lines like +something:data will appear
+    uint8_t lines[MAX_STRING_COUNTER][MAX_STRING_LENGHT+1];
+}AT_STATUS_DATA;
+extern AT_STATUS_DATA at_RESULT; 
+#endif
